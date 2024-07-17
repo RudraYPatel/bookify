9b5b67e541a07413b390772f5171df905f0af27f
RudraYPatel <rudrapatel12241224@gmail.com>
2024-07-16 23:35:47 -0600

    updated home page

--- app/page.js
+++ app/page.js
@@ -41,9 +41,7 @@ export default function Home() {
           <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300">
             <a href="https://www.axs.com/events/541933/ben-platt-tickets" className="text-white text-lg font-semibold">Ben Platt Concert</a>
           </div>
-          <div>
-            <p>Ben Platt</p>
-          </div>
+          <p>Ben Platt</p>
         </div>
         </div>
     </main>
